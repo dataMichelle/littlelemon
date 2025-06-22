8c3e79e249c8ea8c25f6cd08ffcaad2bc6afa294
dataMichelle <data_Michelle@proton.me>
2025-06-22 10:55:44 -0500

    updated DRF auth-token apis

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from LittleLemonAPI import views
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
 router.register(r'users', views.UserViewSet)
@@ -14,4 +15,6 @@ urlpatterns = [
 
     path('auth/', include('djoser.urls')),  # Djoser routes for user management
     path('auth/', include('djoser.urls.authtoken')),  # Djoser token auth routes
+    path('api-token-auth/', obtain_auth_token), # DRF token auth endpoint
+
 ]
